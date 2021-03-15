ea0e9922c2440cc686dd90a4fdf92b35d57b6e57
jollyjunkaih <bluekksailor@gmail.com>
2021-03-15 21:45:02 +0800

    add produce section fucked up. Sunny's advice: take it slow and build ontop of each thing slowly. Building new keyboardscrollview in marketplace. Jiayous!

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import Farm from '_scenes/farm';
 import FDashboard from '_scenes/farmer-dashboard';
 import Order from '_scenes/farmer-order';
-import { createStackNavigator } from '@react-navigation/stack';
+import AddProduce from '_scenes/add_produce';
+import {createStackNavigator} from '@react-navigation/stack';
+import MarketPlace from '_scenes/marketplace';
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const App = () => {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name = "fdashboard" component = {FDashboard}/>
-                <Stack.Screen name = "farm" component = {Farm}/>
-                <Stack.Screen name = "forder" component = {Order}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="marketplace" component={MarketPlace} />
+        <Stack.Screen name="addproduce" component={AddProduce} />
+        <Stack.Screen name="fdashboard" component={FDashboard} />
+        <Stack.Screen name="farm" component={Farm} />
+        <Stack.Screen name="forder" component={Order} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 export default App;
